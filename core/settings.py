f21b5357667f03cfe87d76739af9c7f8f2c738ad
Benjamin Nyakambangwe <benjaminnyakambangwe@gmail.com>
2024-09-07 06:09:25 +0200

    mv to win10

--- core/settings.py
+++ core/settings.py
@@ -131,6 +131,10 @@ STATIC_URL = '/static/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
+DOMAIN = 'http://localhost:3000'
+
+SITE_NAME = 'ROJA ACCOMODATION' 
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
