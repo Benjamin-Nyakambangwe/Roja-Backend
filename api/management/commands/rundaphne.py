98078bab894949fb822a1a53c8067e466ded6c3f
= <=>
2024-10-27 15:51:44 +0200

    ignore

--- api/management/commands/rundaphne.py
+++ api/management/commands/rundaphne.py
@@ -1,3 +1,17 @@
+# from django.core.management.base import BaseCommand
+# from daphne.cli import CommandLineInterface
+
+# class Command(BaseCommand):
+#     help = 'Runs the server with Daphne'
+
+#     def handle(self, *args, **options):
+#         cli = CommandLineInterface()
+#         cli.run(['core.asgi:application'] + list(args))
+
+
+
+
+
 from django.core.management.base import BaseCommand
 from daphne.cli import CommandLineInterface
 
